5fefaa985e3856da081313c85620c07b64078190
priana13 <herian.sap@gmail.com>
2022-10-20 22:08:46 +0700

    pindahkan sidebar ke sidebarleft

--- page/formubahusersetting.php
+++ page/formubahusersetting.php
@@ -44,88 +44,9 @@ include '../elemen/header.php';?>
 <div class="site-menubar-body" >
 <div>
   <div><br><br>
-    <ul class="site-menu" data-plugin="menu"> 
-      <li class="site-menu-item has-sub ">
-        <a href="dashboard.php" class="animsition-link">
-                    <i class="site-menu-icon wb-dashboard" aria-hidden="true"></i>
-                    <span class="site-menu-title">DASHBOARD</span>
-                </a> 
-      </li>              
-      <li class="site-menu-item has-sub">
-        <a href="javascript:void(0)">
-                <i class="site-menu-icon wb-table" aria-hidden="true"></i>
-                <span class="site-menu-title">CONTROL TABLES</span>
-                        <span class="site-menu-arrow"></span>
-            </a>
-            <ul class="site-menu-sub">
-          <li class="site-menu-item">
-            <a class="animsition-link" href="controltablebody1.php">
-              <span class="site-menu-title">Body Plant 1</span>
-            </a>
-          </li>
-          <li class="site-menu-item">
-            <a class="animsition-link" href="controltablebody2.php">
-              <span class="site-menu-title">Body Plant 2</span>
-            </a>
-          </li>
-          <li class="site-menu-item">
-            <a class="animsition-link" href="controltablebqc.php">
-              <span class="site-menu-title">BQC</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="site-menu-item has-sub">
-        <a href="tracking.php">
-                <i class="site-menu-icon wb-shopping-cart" aria-hidden="true"></i>
-                <span class="site-menu-title">TRACKING DOCUMENT</span>
-                        <span class="site-menu-tittle"></span>
-            </a>
-      </li>
-      <li class="site-menu-item has-sub  ">
-        <a href="javascript:void(0)">
-                <i class="site-menu-icon fa-database" aria-hidden="true"></i>
-                <span class="site-menu-title">BUDGET</span>
-                      <span class="site-menu-arrow"></span>                        
-            </a>
-        <ul class="site-menu-sub">
-          <li class="site-menu-item ">
-            <a class="animsition-link" href="budgetb1.php">
-              <span class="site-menu-title">Body Plant 1</span>
-            </a>
-          </li>
-          <li class="site-menu-item">
-            <a class="animsition-link" href="budgetb2.php">
-              <span class="site-menu-title">Body Plant 2</span>
-            </a>
-          </li>
-          <li class="site-menu-item">
-            <a class="animsition-link" href="budgetbqc.php">
-              <span class="site-menu-title">BQC</span>
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li class="site-menu-item has-sub active open">
-        <a href="javascript:void(0)">
-                <i class="site-menu-icon wb-user" aria-hidden="true"></i>
-                <span class="site-menu-title">ADMINISTRATOR</span>
-                        <span class="site-menu-arrow"></span>
-            </a>
-            <ul class="site-menu-sub ">
-          <li class="site-menu-item active open">
-            <a class="animsition-link" href="usersetting.php">
-              <span class="site-menu-title">User Setting</span>
-            </a>
-          </li>
-          <li class="site-menu-item active open">
-            <a class="animsition-link" href="">
-              <span class="site-menu-title">Department Setting</span>
-            </a>
-          </li>
-        </ul>
-      </li>                            
-    </ul>
+
+  <?php include '../elemen/sidebarleft.php';?>
+  
 <!-- Site Navbar Search -->
 <?php include '../elemen/sidebar.php';?>        
 <!-- End Site Navbar Search -->
