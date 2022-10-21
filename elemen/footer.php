63b61397322255ea54e0d256b200bd82b284aa72
hasancute1 <hasanaxie2@gmail.com>
2022-10-21 10:12:32 +0700

    jquery separator class uang

--- elemen/footer.php
+++ elemen/footer.php
@@ -200,7 +200,36 @@ $(document).on('click', 'ul li', function() {
     $(this).addClass('active open').siblings.removeClass('active open')
 })
     </script>
-
+    <script>
+$(function() {
+    $(".uang").keyup(function(e) {
+        $(this).val(format($(this).val()));
+    });
+});
+var format = function(num) {
+    var str = num.toString().replace("", ""),
+        parts = false,
+        output = [],
+        i = 1,
+        formatted = null;
+    if (str.indexOf(".") > 0) {
+        parts = str.split(".");
+        str = parts[0];
+    }
+    str = str.split("").reverse();
+    for (var j = 0, len = str.length; j < len; j++) {
+        if (str[j] != ",") {
+            output.push(str[j]);
+            if (i % 3 == 0 && j < (len - 1)) {
+                output.push(",");
+            }
+            i++;
+        }
+    }
+    formatted = output.reverse().join("");
+    return ("" + formatted + ((parts) ? "." + parts[1].substr(0, 2) : ""));
+};
+    </script>
 
 
     </body>
