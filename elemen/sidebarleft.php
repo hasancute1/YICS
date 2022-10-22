c17a78f66196e3f7dc0976a82bb4774aceea4c6b
priana13 <herian.sap@gmail.com>
2022-10-22 20:58:57 +0700

    active menu budget

--- elemen/sidebarleft.php
+++ elemen/sidebarleft.php
@@ -44,14 +44,14 @@ $url_aktive = $uri[0];
             <span class="site-menu-title">TRACKING DOCUMENT</span>
             <span class="site-menu-tittle"></span>
         </a>
-    <li class="site-menu-item has-sub <?= ($url_aktive == "budgetdep1")? "active": ""; ?>">
+    <li class="site-menu-item has-sub open <?= (in_array($url_aktive , ["budgetdep1", "budgetdep2","budgetdep3"]) )? "active": ""; ?>">
         <a href="javascript:void(0)">
             <i class="site-menu-icon fa-database" aria-hidden="true"></i>
             <span class="site-menu-title">BUDGET</span>
             <span class="site-menu-arrow"></span>
         </a>
         <ul class="site-menu-sub">
-            <li class="site-menu-item <?= ($url_aktive == "budgetdep1")? "active": ""; ?>"">
+            <li class="site-menu-item open <?= ($url_aktive == "budgetdep1")? "active": ""; ?>"">
                 <a class=" animsition-link" href="budgetdep1.php">
                 <span class="site-menu-title">Body Plant 1</span>
                 </a>
@@ -61,7 +61,7 @@ $url_aktive = $uri[0];
                 <span class="site-menu-title">Body Plant 2</span>
                 </a>
             </li>
-            <li class="site-menu-item <?= ($url_aktive == "budgetdep3")? "active": ""; ?>"">
+            <li class="site-menu-item <?= ($url_aktive == "budgetdep3")? "active": ""; ?>">
                 <a class=" animsition-link" href="budgetdep3.php">
                 <span class="site-menu-title">BQC</span>
                 </a>
@@ -69,30 +69,30 @@ $url_aktive = $uri[0];
         </ul>
     </li>
     </li>
-    <li class="site-menu-item has-sub">
+    <li class="site-menu-item has-sub open <?= (in_array($url_aktive, ["usersetting","categorysetting","fiscalsetting","kurs_matauang"] ))? "active": ""; ?>">
         <a href="javascript:void(0)">
             <i class="site-menu-icon wb-user" aria-hidden="true"></i>
             <span class="site-menu-title">ADMINISTRATOR</span>
             <span class="site-menu-arrow"></span>
         </a>
         <ul class="site-menu-sub">
-            <li class="site-menu-item">
+            <li class="site-menu-item <?= ($url_aktive == "usersetting")? "active": ""; ?>">
                 <a class="animsition-link" href="usersetting.php">
                     <span class="site-menu-title">User Setting</span>
                 </a>
             </li>
-            <li class="site-menu-item">
+            <li class="site-menu-item <?= ($url_aktive == "categorysetting")? "active": ""; ?>">
                 <a class="animsition-link" href="categorysetting.php">
                     <span class="site-menu-title">Category Setting</span>
                 </a>
             </li>
-            <li class="site-menu-item">
+            <li class="site-menu-item <?= ($url_aktive == "fiscalsetting")? "active": ""; ?>">
                 <a class="animsition-link" href="fiscalsetting.php">
                     <span class="site-menu-title">Time Fiscal Setting</span>
                 </a>
             </li>
 
-            <li class="site-menu-item">
+            <li class="site-menu-item <?= ($url_aktive == "kurs_matauang")? "active": ""; ?>">
                 <a class="animsition-link" href="kurs_matauang.php">
                     <span class="site-menu-title">Kurs Mata Uang Asing</span>
                 </a>
