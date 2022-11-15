55207cce4ef9a9e2d2b3c51d8d62ecac8596d9ab
priana13 <herian.sap@gmail.com>
2022-11-15 22:25:45 +0700

    depart ambil dari tracking ia

--- proses/Search/depart.php
+++ proses/Search/depart.php
@@ -6,7 +6,14 @@ include("../services/notifications.php");
 	
      echo "<option value=''>Pilih Department</option>";
  
-	$query = "SELECT * FROM depart ORDER BY depart ASC";
+	$query = "SELECT depart.* FROM tracking_ia 
+	JOIN  ia on tracking_ia.id_ia = ia.id_ia
+    JOIN proposal on ia.id_prop = proposal.id_prop
+	JOIN depart ON proposal.id_dep = depart.id_dep
+	GROUP BY id_dep	
+	ORDER BY depart ASC";
+
+
 	$dewan1 = $link_yics->prepare($query);
 	$dewan1->execute();
 	$res1 = $dewan1->get_result();
